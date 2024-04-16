d6beed49bc1ac855625a7edb43ea9985a0e11939
Ashish Singh <ashishsingh822003@gmail.com>
2024-04-16 22:04:33 +0530

    commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require("cors");
+
+app.use(cors("*"));
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(4000, () => {
+  console.log(`server is listening on port ${4000}`);
+});
