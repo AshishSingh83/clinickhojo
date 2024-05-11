1bd8a041f580fb6f5bb5be5668bbd29fc1e6fd92
Ashish Singh <ashishsingh822003@gmail.com>
2024-05-11 19:16:22 +0530

    new

--- src/pages/AdminLogin/SubAdminLogin.jsx
+++ src/pages/AdminLogin/SubAdminLogin.jsx
@@ -3,8 +3,8 @@ import Login from "./Login";
 export default function SubAdminLogin() {
   return (
     <div className=" w-screen h-screen flex justify-center items-center bg-[#0529BB]">
-    <div  className="bg-[#0529BB] w-[600px] " >
-      <div className="m-20 pt-12 pb-12 p-5 ">
+    <div  className="bg-[#0529BB] w-[600px]  " >
+      <div className="m-20 pt- pb-12 p-5 ">
       <Header
           heading="Subadmin Login"
           paragraph="Don't have an account yet? "
